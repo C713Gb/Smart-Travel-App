01980a84357879fd8f7a533f46c3a51014014b8a
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2021-02-06 15:18:08 +0530

    splash screen updated. sharedpreference added

---
+++ app/src/main/java/com/example/smarttravel/Activities/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.smarttravel.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.smarttravel.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int WELCOME_TIMEOUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+            startActivity(intent);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            finish();
+        }, WELCOME_TIMEOUT);
+
+    }
+}
